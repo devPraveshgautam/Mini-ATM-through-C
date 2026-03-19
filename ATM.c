f261582ed185a3f27f5ffdcac217f7b9a5340a27
Pravesh Gautam <praveshgautamofficial@gmail.com>
2026-03-19 21:05:36 +0545

    Add files via upload

---
+++ ATM.c
@@ -0,0 +1,33 @@
+#include<stdio.h>
+#include<stdbool.h>
+#include<ctype.h>
+int main() {
+	int balance = 12345,withdrawAmount,depositeAmount;
+	char userInput;
+	bool entrance = true;
+	printf("------Prashant Bank and Co.------\n\n");
+	while(entrance) {
+		printf("\n");
+		printf("current balnce: %d\n",balance);
+		printf("enter w for withdraw, d for deposite, e for exit: ");
+		scanf(" %c",&userInput);
+		userInput = tolower(userInput);
+		if(userInput == 'w') {
+			printf("Enter the amount for withdraw: ");
+			scanf("%d",&withdrawAmount);
+			balance -= withdrawAmount;
+		}else if(userInput == 'd') {
+			printf("Enter the amount for deposite: ");
+			scanf("%d", &depositeAmount);
+			balance += depositeAmount;
+		}else if(userInput == 'e') {
+			printf("Exited");
+			entrance = false;
+			break;
+		} else {
+			printf("invalid input\n\n");
+		}
+	}
+	return 0;
+	
+}
